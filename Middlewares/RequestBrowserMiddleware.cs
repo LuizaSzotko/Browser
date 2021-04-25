0df848e994232df267e8efd1e2dc7bc4a540fcb5
LuizaSzotko <luizaszotko@gmail.com>
2021-04-25 18:04:41 +0200

    Dodaj pliki projektów.

---
+++ Middlewares/RequestBrowserMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UAParser;
+
+namespace Browser.Middlewares
+{
+    public class RequestBrowserMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RequestBrowserMiddleware (RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync (HttpContext context)
+        {
+            string UA = context.Request.Headers["User-Agent"];
+            string browser = "";
+            string message = "";
+            var uaParser = Parser.GetDefault();
+
+            ClientInfo c = uaParser.Parse(UA);
+            browser = c.UA.Family + " " + c.UA.Major + "." + c.UA.Minor;
+           
+            if (browser.Contains("Edge") || browser.Contains("IE"))
+            {
+                message = "Niestety, Twoja przegladarka nie jest obslugiwana.";
+                await context.Response.WriteAsync(message);
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ Middlewares/RequestBrowserMiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Browser.Middlewares
+{
+    public static class RequestBrowserMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestBrowser(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestBrowserMiddleware>();
+        }
+    }
+}
